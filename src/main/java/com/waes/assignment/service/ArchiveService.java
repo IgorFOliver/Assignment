be64150cf3c421a3f1e074491e79aedaf23b4c98
IgorFOliver <igorfoliver@gmail.com>
2019-08-12 15:55:27 -0300

    Update ArchiveService.java

--- src/main/java/com/waes/assignment/service/ArchiveService.java
+++ src/main/java/com/waes/assignment/service/ArchiveService.java
@@ -2,6 +2,10 @@ package com.waes.assignment.service;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,18 +28,33 @@ public class ArchiveService {
 	
 	@Autowired
 	ArchiveRepository repository;
-
+	
+	@Autowired
+	private HttpServletRequest request;
+	
 	Logger logger = LoggerFactory.getLogger(FileUtil.class);
 	
-	
-	public Archive saveFile(Integer id, String base64, SideEnum side, String realPath) {
+	/**
+	 * Will save the file in the Filesystem and in the Database.
+	 * 
+	 * 
+	 * @param id Number of the file
+	 * @param base64 File in base64 encoeded
+	 * @param side Side of the file
+	 * @return saved Archive with all File Informations
+	 */
+	public Archive saveFile(Integer id, String base64, SideEnum side) {
 		try {
-			String filename = realPath + FileUtil.BASE_PATH_IMAGES + File.separator + SideEnum.LEFT.name() + File.separator + id;
+			String realPath = request.getServletContext().getRealPath("/");
+			String filename = realPath + FileUtil.BASE_PATH_IMAGES + File.separator + side + File.separator + id;
 			FileUtil.saveBase64ToFileSystem(filename, base64);
 			Archive archive = new Archive(id, filename, side);
 			return this.repository.save(archive);
 		} catch (IOException e) {
 			logger.trace(">> Error ArchiveService saveBase64ToFileSystem <<" + e.getLocalizedMessage());
+			return null;
+		} catch(Exception e) {
+			logger.trace(">> Error ArchiveService saveFileToDatabase <<" + e.getLocalizedMessage());
 			return null;
 		}
 	
@@ -43,30 +62,54 @@ public class ArchiveService {
 
 	/**
 	 * Method will comapre two diferent files with the same ID and will return if the size are equals or not.
-	 * If the size are equals will return insights where the diffs are.
+	 * If the size are equals will return Position where the files start to be diffed and the length of the file.
 	 * If the size are not equals will return that the size are different.
 	 * If one of the files are not found will return fail status with messsage.
 	 * 
-	 * @param ID
+	 * @param id
 	 * @param realPath
 	 * @return ComparisonResultVO
 	 */
-	public ComparisonResultVO compareFiles(Integer ID, String realPath) {
-		String path = realPath + FileUtil.BASE_PATH_IMAGES + File.separator + SideEnum.LEFT + File.separator + ID;
-		File leftFile = FileUtil.recoveryFile(path);
-		path = realPath + FileUtil.BASE_PATH_IMAGES + File.separator + SideEnum.RIGHT + File.separator + ID;
-		File rightFile = FileUtil.recoveryFile(path);
+	public ComparisonResultVO compareFiles(Integer id) {
+		Archive leftArchive = this.findFileByFileNumberAndSide(id, SideEnum.LEFT);
+		Archive rightArchive = this.findFileByFileNumberAndSide(id, SideEnum.RIGHT);
+		
+		File leftFile = FileUtil.recoveryFile(leftArchive.getPath());
+		File rightFile = FileUtil.recoveryFile(rightArchive.getPath());
 		
 		ComparisonResultVO comparisonResult = new ComparisonResultVO();
 
 		if(leftFile.exists() && rightFile.exists()) {
-			Long leftSize = leftFile.length();
-			Long rightSize = rightFile.length();
-			comparisonResult.setStatus(StatusEnum.SUCCESS);
-			if(leftSize.equals(rightSize)) {
-				comparisonResult.setMessage("Both files are the same size");
-			} else {
-				
+			try {
+				byte[] leftBytes = Files.readAllBytes(leftFile.toPath());
+				byte[] rightBytes = Files.readAllBytes(rightFile.toPath());
+				if(Arrays.equals(leftBytes, rightBytes)) {
+					comparisonResult.setStatus(StatusEnum.SUCCESS);
+					comparisonResult.setMessage("The files are the same");
+					return comparisonResult;
+				}
+			
+			
+				Long leftSize = leftFile.length();
+				Long rightSize = rightFile.length();
+				comparisonResult.setStatus(StatusEnum.SUCCESS);
+				if(!leftSize.equals(rightSize)) {
+					comparisonResult.setMessage("The files are not the same size");
+				} else {
+					for(int pos = 0; pos < leftBytes.length; pos++ ) {
+						if(leftBytes[pos] != rightBytes[pos]) {
+							comparisonResult.setLenght(leftSize);
+							comparisonResult.setOffset(new Long(pos));
+							comparisonResult.setMessage("The files are differente from position " + pos);
+							return comparisonResult;
+						}
+					}
+				}
+			
+			} catch (IOException e) {
+				comparisonResult.setStatus(StatusEnum.FAIL);
+				comparisonResult.setMessage("Fail to load the files");
+				return comparisonResult;
 			}
 			
 		} else {
@@ -75,6 +118,10 @@ public class ArchiveService {
 		}
 		return comparisonResult;
 		
+	}
+
+	public Archive findFileByFileNumberAndSide(Integer id, SideEnum side) {
+		return this.repository.findByFileNumberAndSide(id, side);
 	}
 
 
