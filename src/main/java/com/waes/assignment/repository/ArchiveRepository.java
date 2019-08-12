0f7b15dcb26b342c57c6c0ef725122ffcfbbc84e
IgorFOliver <igorfoliver@gmail.com>
2019-08-12 15:56:23 -0300

    Update ArchiveRepository.java

--- src/main/java/com/waes/assignment/repository/ArchiveRepository.java
+++ src/main/java/com/waes/assignment/repository/ArchiveRepository.java
@@ -4,10 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.waes.assignment.entity.Archive;
+import com.waes.assignment.enumarator.SideEnum;
 
 @Repository
 public interface ArchiveRepository extends CrudRepository<Archive, Long> {
-	
-	
+
+	Archive findByFileNumberAndSide(Integer id, SideEnum side);
 
 }
