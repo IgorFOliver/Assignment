e0dd2a3c43b00be8de7785b5d7ac7c3db0b6eb06
IgorFOliver <igorfoliver@gmail.com>
2019-08-12 16:00:21 -0300

    Add files via upload

--- src/main/java/com/waes/assignment/data/ArchiveVO.java
+++ src/main/java/com/waes/assignment/data/ArchiveVO.java
@@ -26,4 +26,12 @@ public class ArchiveVO {
 	@JsonProperty(value = "base64")
 	private String base64;
 
+	public String getBase64() {
+		return base64;
+	}
+
+	public void setBase64(String base64) {
+		this.base64 = base64;
+	}
+
 }
--- src/main/java/com/waes/assignment/data/ComparisonResultVO.java
+++ src/main/java/com/waes/assignment/data/ComparisonResultVO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import com.waes.assignment.enumarator.StatusEnum;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,6 +20,22 @@ import lombok.Setter;
 public class ComparisonResultVO {
 	
 	
+	public StatusEnum getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusEnum status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
 	/**
 	 *	Status of Service 
 	 */
@@ -30,7 +47,39 @@ public class ComparisonResultVO {
 	 * Result of comparing two files
 	 */
 	@Getter @Setter
-	@JsonProperty(value =  "result")
+	@JsonProperty(value =  "message")
+    @ApiModelProperty(name = "message", value = "Informative message with the result of comparison")
 	private String message; 
+	
+	
+	@Getter @Setter
+	@JsonProperty(value = "lenght")
+	@ApiModelProperty(name = "lenght", value = "Lenght of the files array")
+	private Long lenght;
+	
+	
+	@Getter @Setter
+	@JsonProperty(value = "offset")
+	@ApiModelProperty(name = "offset", value = "Position in file where difference starts")
+	private Long offset;
+
+
+	public Long getLenght() {
+		return lenght;
+	}
+
+	public void setLenght(Long lenght) {
+		this.lenght = lenght;
+	}
+
+	public Long getOffset() {
+		return offset;
+	}
+
+	public void setOffset(Long offset) {
+		this.offset = offset;
+	}
+	
+	
 
 }
